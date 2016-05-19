b96bdf5fe9f2a882711412337c838190b39b7c15
Anna <annsta@gmail.com>
2016-05-20 02:00:12 +0300

    Advanced C#

---
+++ 01 Stacks and Queues/01 Reverse Numbers/01 Reverse Numbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Numbers
+{
+    class Reverse_Numbers
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersInput = Console.ReadLine()
+                                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                .Select(int.Parse)
+                                .ToArray();
+            Stack<int> numbers = new Stack<int>();
+            for (int i = 0; i < numbersInput.Length; i++)
+                numbers.Push(numbersInput[i]);
+            for (int i = 0; i < numbers.Count; i++)
+                Console.Write("{0} ",numbers.Pop());            
+        }
+    }
+}
