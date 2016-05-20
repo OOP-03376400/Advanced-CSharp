bab91fbe816d74bd567369d806af2efe546bf1c8
Anna <annsta@gmail.com>
2016-05-20 22:18:46 +0300

    Advanced C#

---
+++ 02 Sets and Dictionaries/09 User Logs/09 User Logs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Logs
+{
+    class User_Logs
+    {
+        static void Main(string[] args)
+        {
+            // username, IPaddress, IPaddress hits		
+            SortedDictionary<string, Dictionary<string, int>> userIPadresses = new SortedDictionary<string, Dictionary<string, int>>();
+            string input = "";
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] data = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string ipAddress = data[0].Substring("IP=".Length, data[0].Length - "IP=".Length);
+                string user = data[2].Substring("user=".Length, data[2].Length - "user=".Length);
+
+                Dictionary<string, int> ipCounts = new Dictionary<string, int>();
+                if (userIPadresses.ContainsKey(user))
+                {
+                    ipCounts = userIPadresses[user];
+                    if (ipCounts.ContainsKey(ipAddress))
+                        ipCounts[ipAddress]++;
+                    else ipCounts[ipAddress] = 1;
+                }
+                else ipCounts[ipAddress] = 1;
+                userIPadresses[user] = ipCounts;
+            }
+            foreach (var userPair in userIPadresses)
+            {
+                string ipLogs = "";
+                foreach (var ipPair in userPair.Value)  // ipAddresses & hits
+                    ipLogs += ipPair.Key + " => " + ipPair.Value + ", ";
+                ipLogs = ipLogs.Substring(0, ipLogs.Length - 2);
+                Console.WriteLine("{0}:\n{1}.", userPair.Key, ipLogs);
+            }
+        }
+    }
+}
---
+++ 02 Sets and Dictionaries/10 Population Counter/10 Population Counter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Population_Counter
+{
+    class Population_Counter
+    {
+        static void Main(string[] args)
+        {
+            // country, city, population
+            Dictionary<string, Dictionary<string, int>> population = new Dictionary<string, Dictionary<string, int>>();
+            // country, total population
+            Dictionary<string, long> countryPopulation = new Dictionary<string, long>();
+
+            string input = "";
+            while ((input = Console.ReadLine()) != "report")
+            {
+                string[] data = input.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+                string city = data[0];
+                string country = data[1];
+                int cityPopulation = int.Parse(data[2]);
+
+                if (!population.ContainsKey(country))
+                {
+                    population[country] = new Dictionary<string, int>();
+                    countryPopulation[country] = 0;
+                }
+                population[country].Add(city, cityPopulation); 
+                countryPopulation[country] += cityPopulation;
+            }
+            var orderedCountryStats = countryPopulation.OrderByDescending(x => x.Value);
+            foreach (var countryStats in orderedCountryStats)
+            {
+                // country & population in desc.order
+                Console.WriteLine("{0} (total population: {1})", countryStats.Key, countryStats.Value); 
+                // city & population in desc.order
+                var orderedCityStats = population[countryStats.Key].OrderByDescending(x => x.Value);
+                foreach (var cityStats in orderedCityStats)
+                    Console.WriteLine("=>{0}: {1}", cityStats.Key, cityStats.Value);
+            }
+        }
+    }
+}
