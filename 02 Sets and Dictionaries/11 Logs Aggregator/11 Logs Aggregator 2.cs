a50ae58f7ed27e3c4e3dd594199749be1357b29a
Anna Stambolieva <astambi@users.noreply.github.com>
2016-05-25 12:43:47 +0300

    Update 11 Logs Aggregator 2.cs

--- 02 Sets and Dictionaries/11 Logs Aggregator/11 Logs Aggregator 2.cs
+++ 02 Sets and Dictionaries/11 Logs Aggregator/11 Logs Aggregator 2.cs
@@ -8,22 +8,22 @@ namespace Dragon_Army
     {
         public static void Main(string[] args)
         {
-			// user, IP address, session duration
-			SortedDictionary<string, SortedDictionary<string, int>> userLogs = new SortedDictionary<string, SortedDictionary<string, int>>();
+	    // user, IP address, session duration
+	    SortedDictionary<string, SortedDictionary<string, int>> userLogs = new SortedDictionary<string, SortedDictionary<string, int>>();
 			
             int n = int.Parse(Console.ReadLine());
             for (int i = 0; i < n; i++)
             {
                 string[] data = Console.ReadLine()
-                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            		.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
                 string ipAddress = data[0];
                 string user = data[1];
                 int sessionDuration = int.Parse(data[2]);
 
                 if (!userLogs.ContainsKey(user))
                     userLogs[user] = new SortedDictionary<string, int>();
-				if (!userLogs[user].ContainsKey(ipAddress))
-					userLogs[user][ipAddress] = 0;
+		if (!userLogs[user].ContainsKey(ipAddress))
+		    userLogs[user][ipAddress] = 0;
                 userLogs[user][ipAddress] += sessionDuration;
             }
             foreach (var userPair in userLogs)
@@ -32,7 +32,7 @@ namespace Dragon_Army
                     userPair.Key, 
                     userPair.Value.Select(x => x.Value).Sum(), 
                     string.Join(", ", 
-						userPair.Value.Select(x => x.Key)));
+			userPair.Value.Select(x => x.Key)));
             }
         }
     }
