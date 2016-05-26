c8509535ba6b3bfc2a6af808201c2ccba39fe805
Anna <annsta@gmail.com>
2016-05-26 13:37:41 +0300

    Advanced C#

--- 02 Sets and Dictionaries/10 Population Counter/10 Population Counter.cs
+++ 02 Sets and Dictionaries/10 Population Counter/10 Population Counter.cs
@@ -11,10 +11,7 @@ namespace Population_Counter
         static void Main(string[] args)
         {
             // country, city, population
-            Dictionary<string, Dictionary<string, int>> population = new Dictionary<string, Dictionary<string, int>>();
-            // country, total population
-            Dictionary<string, long> countryPopulation = new Dictionary<string, long>();
-
+            Dictionary<string, Dictionary<string, long>> population = new Dictionary<string, Dictionary<string, long>>();
             string input = "";
             while ((input = Console.ReadLine()) != "report")
             {
@@ -24,22 +21,19 @@ namespace Population_Counter
                 int cityPopulation = int.Parse(data[2]);
 
                 if (!population.ContainsKey(country))
-                {
-                    population[country] = new Dictionary<string, int>();
-                    countryPopulation[country] = 0;
-                }
-                population[country].Add(city, cityPopulation); 
-                countryPopulation[country] += cityPopulation;
+                    population[country] = new Dictionary<string, long>();
+                population[country][city] = cityPopulation;
             }
-            var orderedCountryStats = countryPopulation.OrderByDescending(x => x.Value);
-            foreach (var countryStats in orderedCountryStats)
+            // population in desc.order, by country & city
+            foreach (var countryStats in population.OrderBy(x => -x.Value.Values.Sum()))
             {
-                // country & population in desc.order
-                Console.WriteLine("{0} (total population: {1})", countryStats.Key, countryStats.Value); 
-                // city & population in desc.order
-                var orderedCityStats = population[countryStats.Key].OrderByDescending(x => x.Value);
-                foreach (var cityStats in orderedCityStats)
-                    Console.WriteLine("=>{0}: {1}", cityStats.Key, cityStats.Value);
+                Console.WriteLine("{0} (total population: {1})",
+                    countryStats.Key,
+                    countryStats.Value.Values.Sum());
+
+                foreach (var cityStats in countryStats.Value.OrderBy(x => -x.Value))
+                    Console.WriteLine("=>{0}: {1}",
+                        cityStats.Key, cityStats.Value);
             }
         }
     }
